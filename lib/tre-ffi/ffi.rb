a6861871ef6c0b6e98883fccc00a7eff20045732
postmodern <postmodern.mod3@gmail.com>
2009-09-23 20:04:29 -0700

    Mapped in more functions from lib-tre 0.8.0.

--- lib/tre-ffi/ffi.rb
+++ lib/tre-ffi/ffi.rb
@@ -1,4 +1,6 @@
 require 'tre-ffi/typedefs'
+require 'tre-ffi/reg_aprox_params'
+require 'tre-ffi/reg_aprox_match'
 
 require 'ffi'
 
@@ -8,10 +10,66 @@ module FFI
 
     ffi_lib 'tre'
 
+    enum :reg_errcode, [
+      :reg_ok,
+      :reg_nomatch,
+      :reg_badpat,
+      :reg_ecollate,
+      :reg_ectype,
+      :reg_eescape,
+      :reg_esubreg,
+      :reg_ebrack,
+      :reg_eparen,
+      :reg_ebrace,
+      :reg_badbr,
+      :reg_espace,
+      :reg_badrpt
+    ]
+
+    REG_EXTENDED = 1
+    REG_ICASE = (REG_EXTENDED << 1)
+    REG_NEWLINE = (REG_ICASE << 1)
+    REG_NOSUB = (REG_NEWLINE << 1)
+
+    REG_BASE = 0
+    REG_LITERAL = (REG_NOSUB << 1)
+    REG_RIGHT_ASSOC = (REG_LITERAL << 1)
+    REG_UNGREEDY = (REG_RIGHT_ASSOC << 1)
+
+    REG_NOTBOL = 1
+    REG_NOTEOL = (REG_NOTBOL << 1)
+
+    REG_APPROX_MATCHER = (REG_NOTEOL << 1)
+    REG_BACKTRACKING_MATCHER = (REG_APPROX_MATCHER << 1)
+
+    RE_DUP_MAX = 255
+
     attach_function :tre_regcomp, [:pointer, :pointer, :int], :int
     attach_function :tre_regncomp, [:pointer, :pointer, :size_t, :int], :int
-    attach_function :tre_regwcomp, [:pointer, :pointer, :int], :int
-    attach_function :tre_regwncomp, [:pointer, :pointer, :size_t, :int], :int
+    attach_function :tre_regerror, [:int, :pointer, :pointer, :size_t], :size_t
     attach_function :tre_regfree, [:pointer], :void
+
+    begin
+      attach_function :tre_regwcomp, [:pointer, :pointer, :int], :int
+      attach_function :tre_regwncomp, [:pointer, :pointer, :size_t, :int], :int
+    rescue FFI::NotFoundError
+      STDERR.puts "warning: libtre was not compiled with wchar support"
+    end
+
+    begin
+      attach_function :tre_regaexec, [:pointer, :pointer, :pointer, RegAproxParams, :int], :int
+      attach_function :tre_reganexec, [:pointer, :pointer, :size_t, :pointer, RegAproxParams, :int], :int
+
+      begin
+        attach_function :tre_regawexec, [:pointer, :pointer, :pointer, RegAproxParams, :int], :int
+        attach_function :tre_regawnexec, [:pointer, :pointer, :size_t, :pointer, RegAproxParams, :int], :int
+      rescue FFI::NotFoundError
+        STDERR.puts "warning: libtre was not compiled with wchar support"
+      end
+
+      attach_function :tre_regaparams_default, [:pointer], :void
+    rescue FFI::NotFoundError
+      STDERR.puts "warning: libtre was not compiled with approximate matching support"
+    end
   end
 end
